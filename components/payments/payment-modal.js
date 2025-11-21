f7c8d6bb2479d50dde4cf12c19be4105692a65b4
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-11-21 12:02:07 -0600

    fix bug showing all users cc payments and fixed bug with expiration dates

--- components/payments/payment-modal.js
+++ components/payments/payment-modal.js
@@ -2,35 +2,55 @@ import { useRef } from "react"
 import { Input } from "../form-elements"
 import Modal from "../modal"
 
-export default function AddPaymentModal({ showModal, setShowModal, addNewPayment }) {
-  const merchantNameInput = useRef()
-  const acctNumInput = useRef()
-  return (
-    <Modal showModal={showModal} setShowModal={setShowModal} title="Add New Payment Method">
-      <>
-        <Input
-          id="merchantName"
-          type="text"
-          label="Merchant Name"
-          refEl={merchantNameInput}
-        />
-        <Input
-          id="accNum"
-          type="text"
-          label="Account Number"
-          refEl={acctNumInput}
-        />
-      </>
-      <>
-        <button
-          className="button is-success"
-          onClick={() => addNewPayment({
-            acctNumber: acctNumInput.current.value,
-            merchant: merchantNameInput.current.value
-          })}
-        >Add Payment Method</button>
-        <button className="button" onClick={() => setShowModal(false)}>Cancel</button>
-      </>
-    </Modal>
-  )
+export default function AddPaymentModal({
+	showModal,
+	setShowModal,
+	addNewPayment
+}) {
+	const merchantNameInput = useRef()
+	const acctNumInput = useRef()
+	const expirationDateInput = useRef(0)
+	return (
+		<Modal
+			showModal={showModal}
+			setShowModal={setShowModal}
+			title="Add New Payment Method">
+			<>
+				<Input
+					id="merchantName"
+					type="text"
+					label="Merchant Name"
+					refEl={merchantNameInput}
+				/>
+				<Input
+					id="accNum"
+					type="text"
+					label="Account Number"
+					refEl={acctNumInput}
+				/>
+				<Input
+					id="expiration_date"
+					type="date"
+					label="Expiration Date"
+					refEl={expirationDateInput}
+				/>
+			</>
+			<>
+				<button
+					className="button is-success"
+					onClick={() =>
+						addNewPayment({
+							acctNumber: acctNumInput.current.value,
+							merchant: merchantNameInput.current.value,
+							expiration_date: expirationDateInput.current.value
+						})
+					}>
+					Add Payment Method
+				</button>
+				<button className="button" onClick={() => setShowModal(false)}>
+					Cancel
+				</button>
+			</>
+		</Modal>
+	)
 }
